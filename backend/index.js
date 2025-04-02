ee70052380f6fed576d062d6c9e2849017257521
Nandhini K <107359648+Nandhini0144@users.noreply.github.com>
2025-04-02 22:15:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const userRoutes=require('./routes/userRoutes');
 const {notFound,errorHandler}=require("./middleware/errormiddleware") 
 
 const app=express();
-mongoose.connect("mongodb+srv://nandhinikuppuraj:Nand%400144@cluster1.lwgfisv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1");
+mongoose.connect("mongodb+srv://nandhinikuppuraj:abc123@cluster1.lwgfisv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1");
 mongoose.connection.on('connected',()=>console.log('connected'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
